b711d38bfd5d7e78f71964b88b354ebedd82f97b
YotaroSeki <youyoutarou.dog0115@gmail.com>
2019-06-18 14:22:06 +0900

    Add js lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,7 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'eslint:recommended',
     'plugin:vue/recommended'
   ],
   // required to lint *.vue files
@@ -17,5 +16,7 @@ module.exports = {
     'vue'
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'no-console': 'warn'
+  }
 }
