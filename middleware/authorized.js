752d6232facb4fd3eb8946567a3736bffad7118c
gihachi <38201016+gihachi@users.noreply.github.com>
2019-07-03 14:49:19 +0900

    modify authorized.js

--- middleware/authorized.js
+++ middleware/authorized.js
@@ -1,9 +1,27 @@
 import auth from '../plugins/auth'
 
-export default async function ({ redirect }) {
+export default async function ({ route, redirect }) {
 
-  let user = await auth();
-  if(!user){
+  let firebaseUser = await auth();
+  if(!firebaseUser){
     return redirect('/login')
+  }
+
+  let storedUserInfo = null;
+  // storeからユーザ情報を取得する関数
+  // storedUserInfo = getUserFromStore()
+  if(!storedUserInfo){
+    // apiサーバーからユーザを取得する関数
+    // storedUserInfo = getUserFromServer()
+  }
+
+  // profile登録が済んでいない場合はprofileページに飛ぶ
+  if(!storedUserInfo && route.name != 'profile'){
+    return redirect('/profile')
+  }
+
+  // profile登録が済んでいる時はprofileページに飛べない
+  if(storedUserInfo && route.name == 'profile'){
+    return redirect('/')
   }
 }
