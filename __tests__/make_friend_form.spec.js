6085e3323c59f01a3edc88858154bb8e0efb4b85
gihachi <38201016+gihachi@users.noreply.github.com>
2019-07-14 19:22:30 +0900

    add test of MakeFriendForm

---
+++ __tests__/make_friend_form.spec.js
@@ -0,0 +1,49 @@
+import {mount, createLocalVue} from '@vue/test-utils'
+import MakeFriendForm from '../components/MakeFriendForm'
+import Vuex from 'vuex'
+import ElementUI from 'element-ui'
+import makeAuthHeaderBody from '../plugins/id-token'
+
+const localVue = createLocalVue();
+localVue.use(ElementUI);
+localVue.use(Vuex)
+jest.mock('../plugins/id-token')
+makeAuthHeaderBody.mockResolvedValue({})
+
+describe('validation test', () => {
+
+  let makeFriendFormWrapper
+
+  beforeEach(() => {
+    makeFriendFormWrapper = mount(MakeFriendForm, {
+      localVue,
+    })
+  })
+
+  /**
+   * inputに値を第二有してvalidation結果を照合する
+   * @param {string} inputValue inputに入力する値 
+   * @param {boolean} isValid 入力がvalidかどうか(trueならvalid)
+   * @param {} done jestにテストの終了を通知するdoneコールバック
+   */
+  const checkInputValue = (inputValue, isValid, done) => {
+    const input = makeFriendFormWrapper.find('input')
+    input.setValue(inputValue)
+    makeFriendFormWrapper.vm.$refs['makeFriendForm'].validate((valid) => {
+      expect(valid).toBe(isValid)
+      done()
+    })
+  }
+
+  test('empty is invalid', (done) => {
+    checkInputValue('', false, done)
+  })
+
+  test('input value is invalid if it contains other than work charactor or digit' , (done) => {
+    checkInputValue('@bbb@', false, done)
+  })
+
+  test('valid input' , (done) => {
+    checkInputValue('user', true, done)
+  })
+})
