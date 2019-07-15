639fb248cf5138554e77665225594e0767b6b495
gihachi <38201016+gihachi@users.noreply.github.com>
2019-07-15 16:50:15 +0900

    modify template

--- layouts/AuthPage.vue
+++ layouts/AuthPage.vue
@@ -1,17 +1,33 @@
 <template>
   <el-container>
-    <el-header>
-      <el-dropdown trigger="click">
-        <i 
-          class="el-icon-menu" 
-          style="margin-right: 15px" />
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item @click.native="signout">
-            signout
-          </el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-    </el-header>
+    <el-aside width="170px">
+      <div 
+        v-if="currentUser" 
+        id="profile-wrapper">
+        <p id="user-name">{{ currentUser.name }}</p>
+        <p id="user-id">ID: {{ currentUser.searchID }}</p>
+      </div>
+      <el-menu>
+        <template v-if="currentUser">
+          <el-menu-item @click="makeMeshi">
+            <i class="el-icon-dish"/>
+            <span>make meshi</span>
+          </el-menu-item>
+          <el-menu-item @click="makeFriend">
+            <i class="el-icon-user"/>
+            make friend
+          </el-menu-item>
+          <el-menu-item @click="userSetting">
+            <i class="el-icon-setting"/>
+            user setting
+          </el-menu-item>
+        </template>
+        <el-menu-item @click="signout">
+          <i class="el-icon-switch-button"/>
+          signout
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
     <el-main>
       <nuxt/>
     </el-main>
@@ -45,8 +61,17 @@ export default{
       }).catch(() => {
         this.$message.error('you cannot logout')
       })
+    },
+    makeMeshi(){
+      this.$message.success('meshi is created')
+    },
+    makeFriend(){
+      this.$message.success('make friend')
+    },
+    userSetting(){
+      this.$message.success('user setting')
     }
-  }
+  },
 }
 </script>
 
@@ -67,41 +92,22 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+.el-menu{
+  height: 80vh;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+#user-name{
+  font-size: 24px;
+  font-weight: bold;
 }
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+#user-id{
+  font-size: 18px;
+  color: #a9a9a9;
 }
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-
-.el-header {
-  background-color: #bcffdd;
-  color: #333;
-  line-height: 60px;
-  text-align: right; 
-  font-size: 12px;
+#profile-wrapper{
+  padding: 20px;
+  border-right: solid 1px #e6e6e6;
+  border-bottom: solid 1px #e6e6e6;
 }
 </style>
 
