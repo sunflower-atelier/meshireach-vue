cb3b9d483d20a2ca9227b4a01e42231f80ac51e1
gihachi <38201016+gihachi@users.noreply.github.com>
2019-07-17 20:53:48 +0900

    add test of EventForm

---
+++ __tests__/event_form.spec.js
@@ -0,0 +1,34 @@
+import {mount, createLocalVue} from '@vue/test-utils'
+import EventForm from '../components/EventForm'
+import ElementUI, { Input } from 'element-ui'
+import moment from 'moment'
+
+const localVue = createLocalVue();
+localVue.use(ElementUI);
+
+describe("validation test" , () => {
+
+  let eventFormWrapper
+  beforeEach(() => {
+    eventFormWrapper = mount(EventForm, {
+      localVue,
+    })
+  })
+
+  test('empty title is invalid',  () => {
+    const titleInput = eventFormWrapper.findAll('input').at(0)
+    titleInput.setValue('')
+    titleInput.trigger('blur')
+    expect(eventFormWrapper.find('.is-error').exists())
+  })
+
+  test('past time is invalid', () => {
+    const dateInput = eventFormWrapper.findAll('input').at(0)
+    const timeInput = eventFormWrapper.findAll('input').at(1)
+    const now = moment()
+    dateInput.setValue(now.format('YYYY-MM-DD'))
+    timeInput.setValue('00:00')
+    timeInput.trigger('blur')
+    expect(eventFormWrapper.find('.is-error').exists())
+  })
+})
