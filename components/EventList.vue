b65f125390b4b533412ed31d9d30da1832ba0f6a
gihachi <38201016+gihachi@users.noreply.github.com>
2019-07-25 18:54:56 +0900

    add function for getting events

--- components/EventList.vue
+++ components/EventList.vue
@@ -35,10 +35,24 @@
 </template>
 
 <script>
+import makeAuthHeaderBody from '../plugins/id-token'
+
 export default {
   data() {
     return{
       events : [{id: 1,title:'a', deadline: '2019 2/2 10:11', owner: 'tanaka'},{id: 2,title:'b', deadline: '2019 2/3 10:11', owner: 'takanashi'},{id: 3,title:'c', deadline: '2019 2/4 10:11', owner: 'takai'}]
+    }
+  },
+  async created(){
+    const authHeaderBody = makeAuthHeaderBody()
+    const res = await this.$axios.get('http://localhost:3000/events', {
+      headers: authHeaderBody
+    }).catch((err) => {
+      this.$message.error('cannot get events')
+      return err.response;
+    })
+    if(res.status == 200){
+      // event listを処理する
     }
   }
 }
