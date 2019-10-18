5ac69dffd8c7b423a77d6acf4f477e0a44a9ce75
gihachi <38201016+gihachi@users.noreply.github.com>
2019-10-18 15:48:12 +0900

    add eslint eslint-disable in sw

--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -5,10 +5,10 @@ importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-messaging.js')
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
-firebase.initializeApp({
+firebase.initializeApp({// eslint-disable-line
   'messagingSenderId': '330143908451'
 })
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = firebase.messaging()
+const messaging = firebase.messaging() // eslint-disable-line
