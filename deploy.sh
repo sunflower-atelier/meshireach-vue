6a62ec9d41319d2d047437942e8ca7844eac5bc7
YotaroSeki <youyoutarou.dog0115@gmail.com>
2019-11-08 15:45:24 +0900

    Enable to deploy from ci

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ssh ubuntu@160.16.220.69 << EOC
+  cd ~/meshireach-vue
+  docker-compose down
+  git fetch origin master
+  git reset --hard origin/master
+  docker-compose -f docker-compose-production.yml build
+  docker-compose -f docker-compose-production.yml run web yarn install
+  docker-compose -f docker-compose-production.yml run web yarn build
+  nohup docker-compose -f docker-compose-production.yml up -d
+EOC
