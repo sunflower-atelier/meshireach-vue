41c834bcadaa7796b99538a640b1bdebac8523d9
aki <akihiko00asumi@gmail.com>
2019-11-29 14:35:41 +0900

    Revert "ダチリストの自動更新"
    This reverts commit 91e4fc97ed3fcd45185a397839bcee640c4da6d0.

--- components/home/FriendList.vue
+++ components/home/FriendList.vue
@@ -1,41 +1,34 @@
 <template>
   <ul id="friend-list">
     <friend-list-item
-      v-for="(friend, index) in friends" 
-      :key="index" 
-      :friend="friend" />
+      v-for="(friend, index) in friends"
+      :key="index"
+      :friend="friend"/>
   </ul>
 </template>
 
 <script>
-import friendListItem from "./FriendListItem"
-import { mapGetters, mapActions } from "vuex"
+import friendListItem from './FriendListItem'
+import { mapGetters, mapActions } from 'vuex'
 export default {
   components: {
     friendListItem
-  },
-  data() {
-    return {
-      intervalId: undefined
-    }
   },
   computed: {
     friends() {
       return this.getFriends
     },
-    ...mapGetters("home", {
-      getFriends: "getFriends"
+    ...mapGetters('home', {
+      getFriends: 'getFriends'
     })
   },
-  beforeDestroy() {
-    clearInterval(this.intervalId)
-  },
   async created() {
-    this.intervalId = setInterval(this.fetchFriendsFromAPIServer, 10000)
     await this.fetchFriendsFromAPIServer()
   },
   methods: {
-    ...mapActions("home", ["fetchFriendsFromAPIServer"])
+    ...mapActions('home', [
+      'fetchFriendsFromAPIServer'
+    ])
   }
 }
 </script>
